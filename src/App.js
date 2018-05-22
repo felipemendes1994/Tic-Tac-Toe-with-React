18a3a0db4e4bfb8e601e08821556daff4f0cfcc1
Felipe Mendes <felipemendes1994@gmail.com>
2018-05-22 20:18:18 -0300

    Added some style adjustments, need some changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
 
     this.state = {
       winner: '',
-      message: ''
+      message: '',
+      turn: 'x'
     }
 
     this.gameState = {
@@ -22,10 +23,15 @@ class App extends Component {
 
   clicked(event){
     if(! this.gameState.board[event.target.dataset.square] && this.gameState.locked === false){
-      event.target.innerText = this.gameState.turn;
+      if(this.gameState.turn === 'x'){
+        event.target.innerHTML = '<i class="fas fa-times"></i>';
+      }else if(this.gameState.turn === 'o'){
+        event.target.innerHTML = '<i class="far fa-circle"></i>';
+      }
 
       this.gameState.board[event.target.dataset.square] = this.gameState.turn;
       this.gameState.turn = this.gameState.turn === 'x' ? 'o' : 'x';
+      this.setState({turn: this.gameState.turn});
       this.gameState.rounds = this.gameState.rounds + 1;
       
       var result = this.checkCombination();
@@ -44,7 +50,6 @@ class App extends Component {
   }
 
   checkCombination(){
-
     var combos = [ [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8] ];
     var board = this.gameState.board;
 
@@ -59,24 +64,52 @@ class App extends Component {
     }
   }
 
+  newMatch(){
+    window.location.reload();
+  }
+
   render() {
 
     return (
-      <div id="game">
-        <div id="head">{this.state.message}</div>
+        <div className="container-fluid bg-dark h-100 clearfix">
+          
+          <div className="container float-left bg-warning col-6 h-100">
+            <div id="board" className="h-100 justify-content-center align-items-center" onClick={(e) => this.clicked(e)}>
+              <div> 
+                <div className="square border-dark text-center align-items-center" data-square="0"></div>
+                <div className="square border-dark text-center align-items-center" data-square="3"></div>
+                <div className="square border-dark text-center align-items-center" data-square="6"></div>
+              </div>
+              <div>  
+                <div className="square border-dark text-center align-items-center" data-square="1"></div>
+                <div className="square border-dark text-center align-items-center" data-square="4"></div>
+                <div className="square border-dark text-center align-items-center" data-square="7"></div>
+              </div> 
+              <div>  
+                <div className="square border-dark text-center align-items-center" data-square="2"></div>
+                <div className="square border-dark text-center align-items-center" data-square="5"></div>
+                <div className="square border-dark text-center align-items-center" data-square="8"></div>
+              </div>
+            </div>
+          </div>
 
-        <div id="board" onClick={(e) => this.clicked(e)}>
-          <div className="square" data-square="0"></div>
-          <div className="square" data-square="1"></div>
-          <div className="square" data-square="2"></div>
-          <div className="square" data-square="3"></div>
-          <div className="square" data-square="4"></div>
-          <div className="square" data-square="5"></div>
-          <div className="square" data-square="6"></div>
-          <div className="square" data-square="7"></div>
-          <div className="square" data-square="8"></div>
+          <div className="container float-left bg-danger col-6 h-100">
+            <div className="h-100 justify-content-center align-items-center options">
+                  <button type="button" className="btn btn-outline-warning btn-block btn-lg disabled">1 Player</button>
+                  <button type="button" className="btn btn-outline-warning btn-block btn-lg active" >2 Players</button>
+                  <br /><br /><br />
+                  <button type="button" className="btn btn-outline-warning btn-block btn-lg" onClick={() => this.newMatch()}>New Match</button>
+                  <br /><br /><br />
+
+                  <h2>Turn: {this.state.turn}</h2>
+                  <br/><br /><br />
+
+                  <h1>{this.state.message}</h1>
+            </div>
+          </div>
+          
+
         </div>
-      </div>
     );
   }
 }
